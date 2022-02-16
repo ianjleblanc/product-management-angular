721386370fc23a62875f1b423723f7cb556553bc
Ian LeBlanc <ijleblanc10@gmail.com>
2022-02-16 07:57:13 -0600

    first commit

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
